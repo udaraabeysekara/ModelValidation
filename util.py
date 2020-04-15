18dec31de2f0ec1ca2c33491d424cea5c667feae
Anushka Abeysekara <u0979082@im-epi354.lan>
2020-04-15 12:01:19 -0600

    Added examples with TSAUG package

---
+++ util.py
@@ -0,0 +1,23 @@
+#
+#
+#
+
+#variable naming conventions
+#1) Every class or function first letter of each word is capital
+#2) For variables do not use capital letters
+#3) If there are more than one word seperate them by _ 
+#4) For clases and functions first letter of each word is capital
+#4) Reference to the current instance of the class allways should be self
+#5) Each class and function should have an explanation
+
+import numpy as np
+class util:
+  def __init__():
+
+
+  def Date_To_Series(series,unit):
+	dt = 
+
+
+
+
